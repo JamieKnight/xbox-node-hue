1ee11bf70902dd5872eaf8864272542a7ffd5139
Jamie Knight <jamie@pluslion.com>
2014-09-13 16:04:39 +0100

    Bad dependency name, utill not utills.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var querystring = require('querystring');
-var utils = require('utils');
+var utils = require('util');
 
 var form = '<html><head><title>Test</title></head><body><form method="post">Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form>';
 
@@ -27,7 +27,7 @@ http.createServer( function(req,res) {
       // request ended -> do something with the data
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
       res.write(form)
-      res.write(utils.inspect(decodedBody));
+      res.write(util.inspect(decodedBody));
       res.write('</body><html>');
       res.end();
       
