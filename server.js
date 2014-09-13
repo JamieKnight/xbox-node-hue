ceedd942858fb941f5e03660baa73aa3564492c6
Jamie Knight <jamie@pluslion.com>
2014-09-13 16:02:17 +0100

    Do something with the data.

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 var http = require('http');
+var querystring = require('querystring');
+var utils = require('utils');
+
+var form = '<html><head><title>Test</title></head><body><form method="post">Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form>';
+
 
 http.createServer( function(req,res) {
 
    var currentTime = new Date();
    console.log('Client called at '+currentTime);
    
+   
+   //process the form
    if (req.method == 'POST') {
-    console.log("[200] " + req.method + " to " + req.url);
-      
+    var fullBody = '';
     req.on('data', function(chunk) {
-      console.log("Received body data:");
-      console.log(chunk.toString());
+      // append the current chunk of data to the fullBody variable
+      fullBody += chunk.toString();
     });
+    
+    req.on('end', function() {
+        
+      // parse the received body data
+      var decodedBody = querystring.parse(fullBody);
+      
+      // request ended -> do something with the data
+      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      res.write(form)
+      res.write(utils.inspect(decodedBody));
+      res.write('</body><html>');
+      res.end();
+      
+      res.end();
+    });
+  
+  } else {
+    
+    //show the form
+    res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+    res.write(form);
+    res.write('</body><html>');
+    
   }
-   
-   
-   
-
-   res.writeHead(200, {'Content-Type':'text/html'});
-   res.write('<html><head><title>Test</title></head>');
-   res.write('<body><form method="post">Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form></body><html>');
-   res.end();
 
 }).listen('8124');
