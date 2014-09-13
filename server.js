44d11662654c9f4b4fdddc81dd2f841122725652
Jamie Knight <jamie@pluslion.com>
2014-09-13 16:52:52 +0100

    Refactor and move form to external file.

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-
-//SERVER For config page.
-
+//dependencies for the server
 var http = require('http');
 var querystring = require('querystring');
 var util = require('util');
 
-var form = '<html><head><title>Test</title></head><body><form method="post">Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form>';
+//dependencies for the controller and hue
+var hue = require("node-hue-api");
+var joystick = new (require('joystick'))(0, 3500, 350);
 
-var color = "green";
+//config hue bridge
+var host = "10.0.1.20",
+    username = "newdeveloper",
+    lampCount = 3,
+    HueApi = hue.HueApi,
+    lightState = hue.lightState,
+    api = new HueApi(host, username),
+    buttons = [],
+    axis = [];
 
-http.createServer( function(req,res) {
+//form HTML
+var form = fs.readFileSync('form.html');
 
-   var currentTime = new Date();
-   console.log('Client called at '+currentTime);
-   
-   
-   //process the form
-   if (req.method == 'POST') {
+// Buttons, maps and colors.
+var buttons = [],
+    colors  = [];
+
+//setup & joystic actions.
+var serverAction = function (req,res) {
+  
+  //process the form
+  if (req.method == 'POST') {
     var fullBody = '';
+    
+    // append the current chunk of data to the fullBody variable
     req.on('data', function(chunk) {
-      // append the current chunk of data to the fullBody variable
       fullBody += chunk.toString();
     });
     
+     // parse the received body data
     req.on('end', function() {
         
-      // parse the received body data
       var decodedBody = querystring.parse(fullBody);
       
       // request ended -> do something with the data
@@ -39,34 +52,22 @@ http.createServer( function(req,res) {
       res.end();
     });
   
-  } else {
-    
+  } else {  
     //show the form
     res.writeHead(200, "OK", {'Content-Type': 'text/html'});
     res.write(form);
     res.write('</body><html>');
     res.end();
   }
+}
 
-}).listen('8124');
+var joysticAction = function(event){
+    if (!event.init && event.value == 1 && (action = buttons[event.number])) {
+      action(event);
+    }
+}
 
-
-//Respond to controller events:
-var hue = require("node-hue-api");
-
-var host = "10.0.1.20",
-    username = "newdeveloper",
-    lampCount = 3;
-
-//setup
-var HueApi = hue.HueApi,
-    lightState = hue.lightState,
-    api = new HueApi(host, username),
-    currentLamp = 1,
-    buttons = [],
-    axis = [];
-
-//helpers
+// Helpers
 var displayResult = function(result) {
     console.log(result);
 };
@@ -75,7 +76,6 @@ var displayError = function(err) {
     console.error(err);
 };
 
-
 var setCurrentLampRGB = function(r, g, b){
     api.setLightState(currentLamp, lightState.create().on().rgb(r, g, b))
        .then(displayResult)
@@ -83,6 +83,7 @@ var setCurrentLampRGB = function(r, g, b){
        .done();
 }
 
+//mapping
 buttons[0] = function() {
   console.log(color);
   if (color == "green") {
@@ -92,11 +93,6 @@ buttons[0] = function() {
   }
 }
 
-var joystick = new (require('joystick'))(0, 3500, 350);
-
-joystick.on('button', function(event){
-  console.log(event);
-  if (!event.init && event.value == 1 && (action = buttons[event.number])) {
-    action(event);
-  }
-});
+//bind server
+http.createServer(severAction).listen('80');
+joystick.on('button', joysticAction);
