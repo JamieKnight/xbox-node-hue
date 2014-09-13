68e173cfd1b348416497ff7861fa46100961badc
Jamie Knight <jamie@pluslion.com>
2014-09-13 16:12:48 +0100

    Console log body, add joystick controls.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+
+//SERVER For config page.
+
 var http = require('http');
 var querystring = require('querystring');
 var util = require('util');
 
 var form = '<html><head><title>Test</title></head><body><form method="post">Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form>';
 
+var color = "green";
 
 http.createServer( function(req,res) {
 
@@ -28,6 +32,9 @@ http.createServer( function(req,res) {
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
       res.write(form)
       res.write(util.inspect(decodedBody));
+      
+      console.log(decodedBody)
+      
       res.write('</body><html>');
       res.end();
     });
@@ -42,3 +49,51 @@ http.createServer( function(req,res) {
   }
 
 }).listen('8124');
+
+/*
+//Respond to controller events:
+var hue = require("node-hue-api");
+
+var host = "10.0.1.20",
+    username = "newdeveloper",
+    lampCount = 3;
+
+//setup
+var HueApi = hue.HueApi,
+    lightState = hue.lightState,
+    api = new HueApi(host, username),
+    currentLamp = 1,
+    buttons = [],
+    axis = [];
+
+//helpers
+var displayResult = function(result) {
+    console.log(result);
+};
+
+var displayError = function(err) {
+    console.error(err);
+};
+
+
+var setCurrentLampRGB = function(r, g, b){
+    api.setLightState(currentLamp, lightState.create().on().rgb(r, g, b))
+       .then(displayResult)
+       .fail(displayError)
+       .done();
+}
+
+buttons[0] = function() {
+  if 
+   setCurrentLampRGB(0, 255, 0);
+}
+
+var joystick = new (require('joystick'))(0, 3500, 350);
+
+joystick.on('button', function(event){
+  console.log(event);
+  if (!event.init && event.value == 1 && (action = buttons[event.number])) {
+    action(event);
+  }
+});
+*/
