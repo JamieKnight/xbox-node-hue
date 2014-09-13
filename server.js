3ca804870339e44c5d920b54697810028ce26d74
Jamie Knight <jamie@pluslion.com>
2014-09-13 15:48:29 +0100

    Fix html again.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ http.createServer( function(req,res) {
    console.log('Client called at '+currentTime);
 
    res.writeHead(200, {'Content-Type':'text/html'});
-   res.write('<html><head><title>Test</title</head>');
+   res.write('<html><head><title>Test</title></head>');
    res.write('<body><form>Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select></form></body><html>');
    res.end();
 
