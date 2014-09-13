44d132212526d26e84de0ff0789aa422ba84f009
Jamie Knight <jamie@pluslion.com>
2014-09-13 18:56:23 +0100

    More debugging code.

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
-var http = require('http');
+var http        = require('http');
 var querystring = require('querystring');
-var util = require('util');
-var fs = require('fs');
-var form = fs.readFileSync('form.html');
+var util        = require('util');
+var fs          = require('fs');
+var form        = fs.readFileSync('form.html');
 
 var Server = function(){
-  this.state = [];
-  http.createServer(this.serverAction).listen('80');
-}
 
-Server.prototype.serverAction = function(req,res) {
-  
-  console.log(this);
-  
-  var state = this.state;
-  
-  console.log(this.state);
+  this.state = [];  
+  http.createServer(function(req,res) {
     
-  //process the form
-  if (req.method == 'POST') {
-    var fullBody = '';
+    console.log(this);
     
-    // append the current chunk of data to the fullBody variable
-    req.on('data', function(chunk) {
-      fullBody += chunk.toString();
-    });
+    var state = this.state;
     
-    // parse the received body data
-    req.on('end', function() {  
-      var decodedBody = querystring.parse(fullBody);
+    console.log(this.state);
       
+    //process the form
+    if (req.method == 'POST') {
+      var fullBody = '';
+      
+      // append the current chunk of data to the fullBody variable
+      req.on('data', function(chunk) {
+        fullBody += chunk.toString();
+      });
+      
+      // parse the received body data
+      req.on('end', function() {  
+        var decodedBody = querystring.parse(fullBody);
+        
+        res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+        res.write(form)
+        res.write(util.inspect(decodedBody));
+        res.end();
+        
+        //state[decodedBody.button] = decodedBody.color;
+      });
+    
+    } else {  
+      //show the form
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-      res.write(form)
-      res.write(util.inspect(decodedBody));
+      res.write(form);
       res.end();
-      
-      //state[decodedBody.button] = decodedBody.color;
-    });
-  
-  } else {  
-    //show the form
-    res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-    res.write(form);
-    res.end();
-  }
+    }
+  }).listen('80');
 }
 
 module.exports = exports = Server;
