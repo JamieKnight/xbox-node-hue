e87dff6dea2cb67917efd1c962e8b5bb1e28caa0
Jamie Knight <jamie@pluslion.com>
2014-09-13 15:42:37 +0100

    Add Server and Config Page
    - splits project into lamps and server component
    - state is shared via a map from event to buttons, buttons to actions
    - server provides interface for configuring some buttons.
    
    Add basic form outline for proof of concept
    
    Fix HTML
    
    Fix html again.
    
    Try accessing POST data.
    
    Form should use POST
    
    Do something with the data.
    
    Bad dependency name, utill not utills.
    
    Fix logic error.
    
    fix typo
    
    Console log body, add joystick controls.
    
    Hook joystick up to config server.
    
    Added Package.json
    
    Lets get managing them dependencies.
    
    Refactor and move form to external file.
    
    Added missing dependency
    
    Fix naming issues.
    
    Added current lamp variable.
    
    Control a button.
    
    Fix typo
    
    fixes more typos.
    
    Improve title, add basic styles.
    
    Prototype action mapper.
    
    Event paths.
    
    Change for debugging.
    
    fixed typo
    
    Logic tweak
    
    Tweak logic
    
    Added value for mapping.
    
    Add X, B, Y to mapping.
    
    Refine mapping, and form.
    
    Fix typo
    
    Clarify debugging.
    
    tweak logic
    
    Output action name for debug.
    
    Mapping improvements.
    
    Refactor
    
    More debug info.
    
    Fix logic issues.
    
    More debugging...
    
    Debugging logic.
    
    Simplify Mapping
    
    Adds lamps modules.
    
    testing lamp abstraction
    
    Tweak syntax
    
    Tweak API.
    
    Fix semantic error.
    
    Fix semantic error.
    
    Fix semantic error.
    
    Add missing property.
    
    Fix finger trouble.
    
    Split server and lamp into separate modules.
    
    Add form variable.
    
    Refactor
    
    Require joystick with setup.
    
    Replace this with lamps.
    
    Refactor
    
    Fix syntax error.
    
    Fix typo
    
    Added debug
    
    Refactor
    
    Fixed typo.
    
    Fix scope issue.
    
    fix state issue
    
    investigating state issues.
    
    Adding debugging.
    
    More debugging code.
    
    Fix scope issue.
    
    Fixing scope issue.
    
    Changed variable.
    
    Attempts to fix scope issue.
    
    Server's not working. Reverting to previous code.
    
    Chasing scope issue
    
    scope test
    
    Scope test
    
    Update state from posted values.
    
    Chasing scope issues.
    
    Test mapping....
    
    Fix typo
    
    Move colour mapping into lamps module.
    
    Fix syntax error.
    
    Add off state, and default map for colours.
    
    Add Y and X to UI.
    
    Correct hidden values.
    
    Wire up white command.
    
    Fix syntax error.
    
    Add type concept to state

---
+++ lamps.js
@@ -0,0 +1,67 @@
+var hue = require("node-hue-api");
+
+var lamps = function(){
+    this.host = "10.0.1.20";
+    this.username = "newdeveloper";
+    this.lampCount = 3;
+    this.currentLamp = 1;
+    this.HueApi = hue.HueApi;
+    this.lightState = hue.lightState;
+    this.api = new this.HueApi(this.host, this.username);
+}
+
+// Helpers
+lamps.prototype.displayResult = function(result) {
+    console.log(result);
+};
+
+lamps.prototype.displayError = function(err) {
+    console.error(err);
+};
+
+lamps.prototype.setCurrentLampRGB = function(color){
+
+  switch (color) {
+    case "green":
+      var r = 0,
+          g = 255,
+          b = 0;
+      break;
+    case "red":
+      var r = 255,
+          g = 0,
+          b = 0;
+      break;
+    case "blue":
+      var r = 0,
+          g = 0,
+          b = 255;
+      break;
+    case "white":
+      var r = 255,
+          g = 255,
+          b = 255;
+      break;
+  }
+  
+  this.api.setLightState(this.currentLamp, this.lightState.create().on().rgb(r, g, b))
+    .then(this.displayResult)
+    .fail(this.displayError)
+    .done();
+}
+
+lamps.prototype.setCurrentLampOff = function() {
+  this.api.setLightState(this.currentLamp, this.lightState.create().off())
+    .then(this.displayResult)
+    .fail(this.displayError)
+    .done();
+}
+
+lamps.prototype.setCurrentLampWhite = function() {
+  this.api.setLightState(this.currentLamp, this.lightState.create().on().white(500, 100))
+    .then(this.displayResult)
+    .fail(this.displayError)
+    .done();
+}
+
+module.exports = exports = lamps;
---
+++ server.js
@@ -0,0 +1,48 @@
+var http        = require('http');
+var querystring = require('querystring');
+var util        = require('util');
+var fs          = require('fs');
+var form        = fs.readFileSync('form.html');
+
+var Server = function(){
+
+  this.state = {
+    'a:press': 'green',
+    'b:press': 'red',
+    'y:press': 'off',
+    'x:press': 'blue',
+  };
+
+  var state = this.state;
+
+  http.createServer(function(req,res) {
+    //process the form
+    if (req.method == 'POST') {
+      var fullBody = '';
+      
+      // append the current chunk of data to the fullBody variable
+      req.on('data', function(chunk) {
+        fullBody += chunk.toString();
+      });
+      
+      // parse the received body data
+      req.on('end', function() {  
+        var decodedBody = querystring.parse(fullBody);
+        state[decodedBody.button] = decodedBody.color;
+        
+        res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+        res.write(form)
+        res.write(util.inspect(decodedBody));
+        res.end();
+      });
+    
+    } else {  
+      //show the form
+      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      res.write(form);
+      res.end();
+    }
+  }).listen('80');
+}
+
+module.exports = exports = Server;
