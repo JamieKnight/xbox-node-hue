69e6718443bc876aa41a6e9b1722d0eb7a632a83
Jamie Knight <jamie@pluslion.com>
2014-09-13 15:51:48 +0100

    Try accessing POST data.

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ http.createServer( function(req,res) {
 
    var currentTime = new Date();
    console.log('Client called at '+currentTime);
+   
+   if (req.method == 'POST') {
+    console.log("[200] " + req.method + " to " + req.url);
+      
+    req.on('data', function(chunk) {
+      console.log("Received body data:");
+      console.log(chunk.toString());
+    });
+  }
+   
+   
+   
 
    res.writeHead(200, {'Content-Type':'text/html'});
    res.write('<html><head><title>Test</title></head>');
-   res.write('<body><form>Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select></form></body><html>');
+   res.write('<body><form>Pressing A will set the color <select name="color"><option value="green">Green</option><option value="blue">Blue</option></select><input type="submit" value="save"></form></body><html>');
    res.end();
 
 }).listen('8124');
