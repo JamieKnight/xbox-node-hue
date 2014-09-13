28bc637d2b69dfeffcb67046bf1d655c5d36879b
Jamie Knight <jamie@pluslion.com>
2014-09-13 18:47:41 +0100

    Refactor

--- server.js
+++ server.js
@@ -4,40 +4,42 @@ var util = require('util');
 var fs = require('fs');
 var form = fs.readFileSync('form.html');
 
-var server = function(){
+var Server = function(){
   
   this.state = [];
+  
+  http.createServer(this.serverAction).listen('80');
+}
 
-  http.createServer(function (req,res) {
+Server.prototype.serverAction = function (req,res) {
     
-    //process the form
-    if (req.method == 'POST') {
-      var fullBody = '';
-      
-      // append the current chunk of data to the fullBody variable
-      req.on('data', function(chunk) {
-        fullBody += chunk.toString();
-      });
-      
-      // parse the received body data
-      req.on('end', function() {  
-        var decodedBody = querystring.parse(fullBody);
-        
-        res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-        res.write(form)
-        res.write(util.inspect(decodedBody));
-        res.end();
-        
-        this.state[0] = decodedBody.color0;
-      });
+  //process the form
+  if (req.method == 'POST') {
+    var fullBody = '';
     
-    } else {  
-      //show the form
+    // append the current chunk of data to the fullBody variable
+    req.on('data', function(chunk) {
+      fullBody += chunk.toString();
+    });
+    
+    // parse the received body data
+    req.on('end', function() {  
+      var decodedBody = querystring.parse(fullBody);
+      
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-      res.write(form);
+      res.write(form)
+      res.write(util.inspect(decodedBody));
       res.end();
-    }
-  }).listen('80');
+      
+      this.state[0] = decodedBody.color0;
+    });
+  
+  } else {  
+    //show the form
+    res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+    res.write(form);
+    res.end();
+  }
 }
 
 module.exports = exports = server;
