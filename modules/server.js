9c24c439ebf25fa0ce6b1f61b4cc109defeac6bc
Jamie Knight <jamie@pluslion.com>
2014-09-14 20:53:14 +0100

    Fixes to server state and form.

--- modules/server.js
+++ modules/server.js
@@ -5,44 +5,51 @@ var fs          = require('fs');
 var form        = fs.readFileSync('form.html');
 
 var Server = function(){
-
   this.state = {};
+  http.createServer(this.request.bind(this)).listen('80');
+}
+
+Server.prototype.getState = function(){
+  return this.state;
+}
+
+Server.prototype.request = function(req,res) {
 
   var state = this.state;
 
-  http.createServer(function(req,res) {
-    //process the form
-    if (req.method == 'POST') {
-      var fullBody = '';
-      
-      // append the current chunk of data to the fullBody variable
-      req.on('data', function(chunk) {
-        fullBody += chunk.toString();
-      });
-      
-      // parse the received body data
-      req.on('end', function() {  
-        var decodedBody = querystring.parse(fullBody);
-        //state[decodedBody.button]['value'] = decodedBody.value;
-        //state[decodedBody.button]['type'] = decodedBody.type;
-        
-      
-        console.log(decodedBody);
-        console.log(state);
-        
-        res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-        res.write(form)
-        res.write(util.inspect(decodedBody));
-        res.end();
-      });
+  //process the form
+  if (req.method == 'POST') {
+    var fullBody = '';
     
-    } else {  
-      //show the form
+    // append the current chunk of data to the fullBody variable
+    req.on('data', function(chunk) {
+      fullBody += chunk.toString();
+    });
+    
+    // parse the received body data
+    req.on('end', function() {  
+      var decodedBody = querystring.parse(fullBody);
+      //state[decodedBody.button]['value'] = decodedBody.value;
+      //state[decodedBody.button]['type'] = decodedBody.type;
+      
+      console.log(state)
+      
+      state[decodedBody.button] = {type: decodedBody.type, value: decodedBody.value}
+      
+      console.log(state)
+      
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-      res.write(form);
+      res.write(form)
+      res.write(util.inspect(decodedBody));
       res.end();
-    }
-  }).listen('80');
+    });
+  
+  } else {  
+    //show the form
+    res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+    res.write(form);
+    res.end();
+  }
 }
 
 module.exports = exports = Server;
