c127424d56719cfb3b934e4541edd3836971cf1a
Jamie Knight <jamie@pluslion.com>
2014-09-14 21:49:43 +0100

    add timer to default map

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ server.state = {
   'ddown:press': {type: 'preset', value: 'lowwhite' },
   'dup:press': {type: 'preset', value: 'highwhite' },
   'dleft:press': {type: 'preset', value: 'pinkish' },
-  'dright:press': {type: 'preset', value: 'midwhite' }
+  'dright:press': {type: 'preset', value: 'timer' }
 }
 
 var eventIsMapped = function(event){
